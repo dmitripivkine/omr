4577285a3a71a2eb8259c9970a5c8bebde142fd9
Dmitri Pivkine <Dmitri_Pivkine@ca.ibm.com>
2025-10-02 16:54:37 -0400

    remove const

--- gc/stats/LargeObjectAllocateStats.hpp
+++ gc/stats/LargeObjectAllocateStats.hpp
@@ -76,7 +76,7 @@ private:
 	uintptr_t _TLHSizeClassIndex; /**< preserved next value of sizeClassIndex on last invocation of simulateAllocateTLHs */
 	uintptr_t _TLHFrequentAllocationSize;/**< preserved next value of FrequentAllocationSize on last invocation of simulateAllocateTLHs */
 
-	const bool _shouldUseIntegerSizeToIndex; /**< should use conversion in integers to calculate index to size and back */
+	bool _shouldUseIntegerSizeToIndex; /**< should use conversion in integers to calculate index to size and back */
 
 	MMINLINE uintptr_t getNextSizeClass(uintptr_t sizeClassIndex, uintptr_t maxSizeClasses);
 	MMINLINE bool isFirstIterationCompleteForCurrentStride(uintptr_t sizeClassIndex, uintptr_t maxSizeClasses);
@@ -300,6 +300,7 @@ public:
 		_maxHeapSize(0),
 		_TLHSizeClassIndex(0),
 		_TLHFrequentAllocationSize(0),
+		_shouldUseIntegerSizeToIndex(false)
 	{
 	}
 
